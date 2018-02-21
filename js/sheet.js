e0957e43b3425febda3e373e2a702baccf2d4b22
glni <glni@easj.dk>
2018-02-21 22:40:24 +0100

    init

---
+++ js/sheet.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(initialize);
+
+    function initialize() {
+        drawPieChart("pieChart");
+        drawLineChart("lineChart");
+    }
+
+    function drawPieChart(HTMLElementId){
+
+    }
+
+    function drawLineChart(HTMLElementId){
+
+    }
+
+});
